2ef2828b7fbb2f4da5676b46ff792b703280fe43
Azaruddin8297 <ajarpopo.com>
2023-06-02 12:19:08 +0530

    newProject

---
+++ FundoNoteApplication/DataLayer/DB/FundoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.DB
+{
+    public class FundoContext : DbContext
+    {
+        public FundoContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+        public DbSet<UserEntity> UserTable { get; set; }
+    }
+}
